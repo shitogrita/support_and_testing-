6418d65959febd7f9e237a13cde921852a15a634
shitogrita <shitotetra@gmail.com>
2025-05-30 00:57:14 +0300

    Update

--- 13/WpfApp2_2/WpfApp2/App.xaml.cs
+++ 13/WpfApp2_2/WpfApp2/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace WpfApp2
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
