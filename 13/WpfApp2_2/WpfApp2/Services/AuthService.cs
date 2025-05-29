35d987b35c3762724450b048bc7785dc6ae865ba
shitogrita <shitotetra@gmail.com>
2025-05-30 00:56:39 +0300

    Update

--- 13/WpfApp2_2/WpfApp2/Services/AuthService.cs
+++ 13/WpfApp2_2/WpfApp2/Services/AuthService.cs
@@ -1,14 +1,10 @@
 ﻿using System.Linq;
-// Пространство имён модели данных
 using WpfApp2;
 
 namespace WpfApp2.Services
 {
     public class AuthService
     {
-        /// <summary>
-        /// Возвращает true если в БД есть user.Login/password
-        /// </summary>
         public bool Authenticate(string login, string password)
         {
             if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
